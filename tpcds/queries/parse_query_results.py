983118edd9559001e651e41bbe71b3821866f4f9
Sounak Gupta <sounak.gupta@intel.com>
2024-02-28 19:48:19 -0500

    Added fixes in the run and parsing scripts

--- tpcds/queries/parse_query_results.py
+++ tpcds/queries/parse_query_results.py
@@ -11,56 +11,76 @@ import csv
 import ntpath
 
 def query_name(path):
-  head, tail = ntpath.split(path)
-  return ntpath.splitext(tail)[0] or ntpath.basename(head)
+    head, tail = ntpath.split(path)
+    return ntpath.splitext(tail)[0] or ntpath.basename(head)
 
 
 def parse(file_name):
-  with open(file_name) as f:
-    data = f.readlines()
 
-  # Split and parse system results
-  metrics = {'query' : query_name(file_name)}
-  seen = False
-  for line in data:
-    if line in ['\n', '\r\n']:
-      continue
+    with open(file_name) as f:
+        data = f.readlines()
 
-    elif "Performance counter stats for" in line:
-      seen = True
-      continue
+    # Split and parse system results
+    metrics = {'query' : query_name(file_name)}
+    seen = False
 
-    elif seen == False:
-      continue
+    for line in data:
 
-    # Split the string by #
-    metricsList = line.split("#")
-    metricsList = [string.strip() for string in metricsList]
-    for metric in metricsList:
-      if len(metric) == 2:
-        raise Exception('List {} size != 2'.format(metric))
-      indv_metric = metric.split(" ", 1)
-      val, key = [string.strip() for string in indv_metric]
-      metrics[key] = val
+        if line in ['\n', '\r\n']:
+            continue
 
-  return metrics
+        elif "Performance counter stats for" in line:
+            seen = True
+            continue
+
+        elif seen == False:
+            continue
+
+        # Split the string by #
+        metricsList = line.split("#")
+        metricsList = [string.strip() for string in metricsList]
+
+        for metric in metricsList:
+            indv_metric = metric.split(" ", 1)
+            val, key = [string.strip() for string in indv_metric]
+            metrics[key] = val
+
+    return metrics
 
 
-def main(path = '/tmp/sf1', pattern = '*.log', csv_file = '/tmp/perf_results.csv'):
-  with open(csv_file, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    header = True
-    for f in glob.glob(path + '/' + pattern):
-      print("Parse {}".format(f))
-      metrics = parse(f)
-      if header == True:
-        keys = list(metrics.keys())
-        writer.writerow(keys)
-        header = False
-      values = list(metrics.values())
-      writer.writerow(values)
+def main( path = '/tmp/sf1',
+          pattern = '*.log',
+          csv_file = '/tmp/sf1/perf_results.csv' ):
+
+    # Ask the user before continuing
+    print("****************************")
+    print("path:     " + path)
+    print("pattern:  " + pattern)
+    print("csv_file: " + csv_file)
+    print("****************************")
+
+    if input("continue? (y/n)") != "y":
+        exit()
+
+    with open(csv_file, 'w', newline='') as csvfile:
+
+        writer = csv.writer(csvfile)
+        header = True
+
+        for f in glob.glob(path + '/' + pattern):
+
+            print("Parse {}".format(f))
+            metrics = parse(f)
+
+            if header == True:
+                keys = list(metrics.keys())
+                writer.writerow(keys)
+                header = False
+
+            values = list(metrics.values())
+            writer.writerow(values)
 
 
 if __name__ == "__main__":
-  fire.Fire(main)
+    fire.Fire(main)
 
--- tpcds/queries/run_queries.py
+++ tpcds/queries/run_queries.py
@@ -5,6 +5,7 @@ import subprocess
 import glob
 import os
 import re
+import shutil
 
 """
 # TODO: Move prepare.sh script to Python
@@ -15,11 +16,11 @@ import re
 Given a query_file run the query on the presto docker container and profile it 
 using perf. The ouput gets stored in output_path.
 """
-def run_and_profile_query(query_path, 
-              output_path, 
-              timeout_in_secs=12000,
-              presto_container="coordinator"):
-    
+def run_and_profile_query( query_path,
+                           output_path,
+                           timeout_in_secs=12000,
+                           presto_container="coordinator"):
+
     perf_command = "perf stat -d" 
     docker_command = "docker exec -it " + presto_container
     presto_command = "presto-cli --file"
@@ -29,19 +30,19 @@ def run_and_profile_query(query_path,
     presto_command = presto_command.split(" ")
 
     command = perf_command + docker_command + presto_command + [query_path]
-  
+
     print("Gathering stats for ", query_path)
 
-    with open(output_path+".out", "w") as out, open(output_path+".log", "w") as err:
+    with open(output_path + ".out", "w") as out, open(output_path+".log", "w") as err:
         proc = subprocess.run(command, 
                               encoding='utf-8', 
                               stderr=err, 
                               stdout=out, 
                               timeout=timeout_in_secs)
-    
+
     if proc.returncode != 0:
         return 1
-    
+
     return 0
 
 """
@@ -49,77 +50,90 @@ Use the prepare script to generat queries for the given scale_factor
 Returns the list of queries generated
 """
 def prepare(scale_factor):
-   
-   proc = subprocess.run(["./prepare.sh", str(scale_factor)])
-   queries = [os.path.basename(x) for x in glob.glob('./generated/q*.sql')]
-   queries.sort()
-   return queries
+    proc = subprocess.run(["./prepare.sh", str(scale_factor)])
+    queries = [os.path.basename(x) for x in glob.glob('./generated/q*.sql')]
+    queries.sort()
+    return queries
+
+"""
+Remove files with a specified pattern from a specified path
+"""
+def delete_files(path, pattern = '*'):
+    for i in glob.glob(os.path.join(path, pattern)):
+        if os.path.isdir(i):
+            shutil.rmtree(i)
+        else:
+            os.remove(i)
 
 """
 Run and profile queries with scale_factor and store the output in output_dir
 """
-
-# TODO Set both drop and clean flags to default value
-def run(scale_factor=1,
-        output_dir="/tmp/sf1/",
-        query_range=range(1,2),
-        drop=True,
-        clean_output_dir=True):
+def run( scale_factor = 1,
+         output_dir_pattern = '/tmp/sf',
+         query_range = '1..2',
+         reload_db = False,
+         clean_output_dir = False ):
 
     # Ask the user before continuing
-    print("*******************")
-    print("scale factor: " + str(scale_factor))
-    print("Output_directory: " + output_dir)
-    print("query_range: " + str(query_range))
-    print("drop: " + str(drop))
-    print("clean_output_dir: " + str(clean_output_dir))
-    print("*******************")
+    print("****************************")
+    print("scale_factor:       " + str(scale_factor))
+    print("output_dir_pattern: " + output_dir_pattern)
+    print("query_range:        " + query_range)
+    print("reload_db:          " + str(reload_db))
+    print("clean_output_dir:   " + str(clean_output_dir))
+    print("****************************")
 
     if input("continue? (y/n)") != "y":
         exit()
 
+    # Create the ouput directory if it does not exist
+    output_dir = output_dir_pattern + str(scale_factor)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    # Check that the ouput directory exists
-    if not os.path.isdir(output_dir):
-        print("output directory does not exist, or the path is incorrect")
-        exit()
-    
     # Clean the output directory
     if clean_output_dir:
-        command = "rm -rf "+output_dir+"*"
-        subprocess.run(command.split(" "))    
-    
-    # Drop the tables
-    if drop:
-        drop_query = "drop_tables.sql"
-        run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/"+ drop_query, 
-                                  output_dir+drop_query.split('.')[0])
+        # Purge all output files
+        delete_files(output_dir, '*.out')
 
-        command = "docker exec minio rm -rf data/tpcds-sf"+str(scale_factor)+"-partitioned-dsdgen-parquet"
+        # Purge all perf stat files
+        delete_files(output_dir, '*.log')
+
+    # Run drop_tables query
+    if reload_db:
+        # Drop the tables
+        drop_query = "drop_tables.sql"
+        run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/" + drop_query, 
+                                  output_dir + "/" + drop_query.split('.')[0])
+
+        command = "docker exec minio rm -rf data/tpcds-sf" + str(scale_factor) + \
+                                  "-partitioned-dsdgen-parquet"
         print(command)
         proc = subprocess.run(command.split(" "))
 
     # Run prepare to set up bucket and schema for the provided scale factor
     queries = prepare(scale_factor)
 
-    
     # Run create_tables query
-    create_query = "create_tables.sql"
-    run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/"+ create_query, 
-                                  output_dir+create_query.split('.')[0])
+    if reload_db:
+        create_query = "create_tables.sql"
+        run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/" + create_query, \
+                                  output_dir + "/" + create_query.split('.')[0])
 
     # Run queries
+    lower, upper = query_range.split("..")
     for query in queries:
-        if int(re.findall(r'\d+', query)[-1]) in query_range:
-            return_code = run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/"+ query, 
-                                  output_dir+query.split('.')[0])
+        if int(re.findall(r'\d+', query)[-1]) in range(int(lower), int(upper)):
+            return_code = run_and_profile_query("./tpcds/sf" + str(scale_factor) + "/" + query, \
+                                  output_dir + "/" + query.split('.')[0])
 
     print("*******************")
-    print("scale factor: " + str(scale_factor))
-    print("Output_directory: " + output_dir)
-    print("query_range: " + str(query_range))
+    print("scale_factor: " + str(scale_factor))
+    print("output_dir:   " + output_dir)
+    print("query_range:  " + query_range)
     print("*******************")
 
-   
+
 if __name__ == '__main__':
-  fire.Fire(run)
+    fire.Fire(run)
+
